0b6aecce7153dd1bdd9abbfd3305f28232f2d359
EvanNauj <evannaujokaitis@yahoo.com>
2026-01-21 15:19:20 -0600

    Fix: improved add_numbers so it should totally work now

--- git_lab.py
+++ git_lab.py
@@ -61,4 +61,4 @@ def is_prime(n):
     return True
 def add_numbers(a, b):
     """Returns the incorrect sum of two numbers."""
-    return a - b  # Introduces an error
+    return a * b  # Introduces an error
